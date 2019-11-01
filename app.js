512cb1257749993a8d5345b11800ce1ba33cff52
joseEstrada <jose.estrada@mqaamericas.com>
2019-11-01 15:04:48 -0600

    Subiendo la aplicacion al repositorio

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+var port = process.env.PORT || 3000;
+app.get('/', (req,res)=>{
+    res.send("Cubo empieza todo")
+});
+
+app.listen(port, ()=>console.log('Server is running dude!! ' + port ));
